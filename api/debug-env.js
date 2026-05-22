f02c0e1ad3686c06efc443eef8b2c608a7f69b75
aidensawyer904-collab <aidensawyer904@gmail.com>
2026-05-22 12:40:57 +0000

    Debug 1

--- api/debug-env.js
+++ api/debug-env.js
@@ -1,8 +1,30 @@
-module.exports = function handler(req, res) {
+'use strict';
+
+module.exports = async function handler(req, res) {
+  const binId  = process.env.JSONBIN_BIN_ID;
+  const apiKey = process.env.JSONBIN_API_KEY;
+
+  const url = `https://api.jsonbin.io/v3/b/${binId}?meta=false`;
+
+  let result, text;
+  try {
+    result = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Master-Key': apiKey,
+      }
+    });
+    text = await result.text();
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+
   res.json({
-    hasBinId: !!process.env.JSONBIN_BIN_ID,
-    hasApiKey: !!process.env.JSONBIN_API_KEY,
-    binIdLength: (process.env.JSONBIN_BIN_ID || '').length,
-    apiKeyLength: (process.env.JSONBIN_API_KEY || '').length,
+    hasBinId:     !!binId,
+    hasApiKey:    !!apiKey,
+    binIdLength:  binId.length,
+    apiKeyLength: apiKey.length,
+    status:       result.status,
+    body:         text,
   });
 };
